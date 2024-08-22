0ae3c1415df3351861f31ff28f352100fd756074
Junior de Oliveira <ellismarjr@hotmail.com>
2024-08-22 09:06:06 -0300

    feat: Add profile route for retrieving user profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import { userRegister } from "./controllers/users/users-register";
 import { authenticate } from "./controllers/auth/authenticate";
+import { profile } from "./controllers/profile/profile";
+import { verifyJwt } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", userRegister);
 	app.post("/sessions", authenticate);
+
+	// Private routes
+	app.get("/me", { onRequest: [verifyJwt] }, profile);
 }
